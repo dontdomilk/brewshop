31d6f4708c314d1c61651449e26129745d6150bb
dontdomilk <jacob@decor-d.com>
2019-12-13 09:23:25 +0200

    modal opening and transitions

--- src/components/modal/beer-card/index.js
+++ src/components/modal/beer-card/index.js
@@ -5,7 +5,7 @@ import Modal from '../index.js';
 const BeerCardModal = (props) => {
     console.log(props)
     return (
-        <Modal open={props.open} toggle={props.toggle}>
+        <Modal open={props.open} toggle={props.toggle} >
             <div className="name">{props.data.name}</div>
         </Modal>
     )
--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
 
-const Modal = ({children, type, id, open}) => {
+const Modal = ({children, type, id, open, toggle}) => {
+
+
 
     const ModalElement = () => {
-        console.log(children)
+        const [isOpen, setOpen] = useState(true);
+
+        const [transition, setTransition] = useState('');
+
+        useEffect(() => {
+            if (isOpen === true) {
+                setTransition('open')
+            } else {
+                setTransition('close')
+            }
+        }, [isOpen]);
+
+        const handleToggle = () => {
+            setOpen(!isOpen);
+            toggle();
+        };
+
         return (
-            <div className="modal-wrapper" >
+            <div className={`modal-wrapper ${transition}`} >
                 <div className="content-wrapper">
+                    <div className="close" onClick={handleToggle}>X</div>
                     {children}
                 </div>
             </div>
